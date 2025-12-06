d2d15134ad26e0fec6e81af0beccba224c495cdf
rajesh <rajeshkaramsetty1999@gmail.com>
2025-12-06 19:49:54 +0530

    added content

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,4 +1,5 @@
-module "sg" {
+#security group for bastion host
+module "bastion" {
   source         = "git::https://github.com/rajesh1816/terraform-sg-module.git?ref=main"
   project        = var.project
   environment    = var.environment
@@ -6,3 +7,35 @@ module "sg" {
   sg_description = var.sg_description
   vpc_id         = data.aws_ssm_parameter.vpc_id.value
 }
+
+#ingress rule for bastion sg
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0"]
+  security_group_id = module.bastion.sg_id
+}
+
+
+#security group for backend alb
+module "backend_alb" {
+  source         = "git::https://github.com/rajesh1816/terraform-sg-module.git?ref=main"
+  project        = var.project
+  environment    = var.environment
+  sg_name        = var.sg_name
+  sg_description = var.sg_description
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+}
+
+#ingress rule for backend alb
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id  = module.bastion.sg_id
+  security_group_id = module.backend_alb.sg_id
+}
+
