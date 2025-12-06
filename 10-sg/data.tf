46586267b18fd412cc82d257ae52f59f53549bf0
rajesh <rajeshkaramsetty1999@gmail.com>
2025-12-06 18:18:40 +0530

    added content

--- 10-sg/data.tf
+++ 10-sg/data.tf
@@ -1,4 +1,4 @@
 data "aws_ssm_parameter" "vpc_id" {
-  name            = "/${var.project}/${var.environment}/vpc"
+  name            = "/${var.project}/${var.environment}/vpc-id"
   with_decryption = true # Set to true for SecureString parameters
 }
