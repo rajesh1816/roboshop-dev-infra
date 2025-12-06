76bc4df28c9fcb0dd773464dd3aacb8160b0416f
rajesh <rajeshkaramsetty1999@gmail.com>
2025-12-06 21:07:41 +0530

    added content

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -1,6 +1,6 @@
 locals {
   vpc_id = data.aws_ssm_parameter.vpc_id.value
-  private_subnet_ids = data.aws_ssm_parameter.private_subnet_ids
+  private_subnet_ids = data.aws_ssm_parameter.private_subnet_ids.value
   backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
 
   common_tags = {
