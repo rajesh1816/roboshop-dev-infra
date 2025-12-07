d306a879d42b6d19aabc9c0525983d7083fbef21
rajesh <rajeshkaramsetty1999@gmail.com>
2025-12-07 21:56:23 +0530

    added content

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+component=$1
 dnf install ansible -y
-ansible pull -U "https://github.com/rajesh1816/ansible-roles-roboshop.git" -e "component=$1" main.yaml
+ansible-pull -U "https://github.com/rajesh1816/ansible-roles-roboshop.git" -e "component=$1" main.yaml
