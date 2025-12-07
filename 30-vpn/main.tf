ded6dc39b0f0ae439334d81f3d7bf7742dfed237
rajesh <rajeshkaramsetty1999@gmail.com>
2025-12-07 13:32:16 +0530

    added content

--- 30-vpn/main.tf
+++ 30-vpn/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "openvpn" {
   ami           = data.aws_ami.openvpn.id
   instance_type = "t3.micro"
 
-  subnet_id              = local.public_subnets[0]
+  subnet_id              = local.public_subnet_ids[0]
   vpc_security_group_ids = [local.vpn_sg_id]
 
   associate_public_ip_address = true
