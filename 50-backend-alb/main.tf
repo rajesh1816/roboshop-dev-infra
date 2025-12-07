7c9102740b28cb1b3fb3b0d47650ec9218168868
rajesh <rajeshkaramsetty1999@gmail.com>
2025-12-07 20:02:26 +0530

    added content

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -1,14 +1,14 @@
 module "backend_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source   = "terraform-aws-modules/alb/aws"
   internal = true
 
-  name    = "${var.project}-${var.environment}-backend-alb"
-  vpc_id  = local.vpc_id
-  subnets = local.private_subnet_ids
-  security_groups = [local.backend_alb_sg_id]
-  create_security_group = false
+  name                       = "${var.project}-${var.environment}-backend-alb"
+  vpc_id                     = local.vpc_id
+  subnets                    = local.private_subnet_ids
+  security_groups            = [local.backend_alb_sg_id]
+  create_security_group      = false
   enable_deletion_protection = false
-  version = "9.16.0"
+  version                    = "9.16.0"
 
   tags = merge(
     var.backend_alb_tags,
