ca388401769851b5570163efe89e488bebb5cd3b
rajesh <rajeshkaramsetty1999@gmail.com>
2025-12-09 20:28:30 +0530

    added content

--- 30-vpn/locals.tf
+++ 30-vpn/locals.tf
@@ -1,6 +1,6 @@
 locals {
   vpc_id            = data.aws_ssm_parameter.vpc_id.value
-  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
   vpn_sg_id         = data.aws_ssm_parameter.vpn_sg_id.value
 
   common_tags = {
--- 30-vpn/main.tf
+++ 30-vpn/main.tf
@@ -1,21 +1,19 @@
-resource "aws_key_pair" "vpn" {
+/* resource "aws_key_pair" "vpn" {
   key_name = "openvpn"
   #public_key = file("C:\\keys\\openvpn.pub") in windows
   public_key = file("C:\\keys\\openvpn.pub") # for linux
-}
-
+} */
 
 
 resource "aws_instance" "openvpn" {
-  ami           = data.aws_ami.openvpn.id
-  instance_type = "t3.micro"
-
-  subnet_id              = local.public_subnet_ids[0]
+  ami                    = "ami-058fbc284998614e2"
+  instance_type          = "t2.small"
+  subnet_id              = local.public_subnet_ids
   vpc_security_group_ids = [local.vpn_sg_id]
-
   associate_public_ip_address = true
+  key_name = "openvpn" # make sure this key exist in AWS
+  #key_name = aws_key_pair.openvpn.key_name
   user_data                   = file("openvpn.sh")
-
   tags = merge(
     var.openvpn_tags,
     local.common_tags,
