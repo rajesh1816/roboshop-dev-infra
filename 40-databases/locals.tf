33738d6754bbe9b5a937b6ad0c8a452544dd1b80
rajesh1816 <rajeshkaramsetty1999@gmail.com>
2025-12-10 10:00:01 +0000

    added content

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -7,6 +7,7 @@ locals {
   redis_sg_id        = data.aws_ssm_parameter.redis_sg_id.value
   rabbitmq_sg_id     = data.aws_ssm_parameter.rabbitmq_sg_id.value
   mysql_sg_id        = data.aws_ssm_parameter.mysql_sg_id.value
+  databases_sg_id        = data.aws_ssm_parameter.databases_sg_id.value
 
 
 
