5037137cafbc3e79fb2109a428e2adb09a4910f6
rajesh1816 <rajeshkaramsetty1999@gmail.com>
2025-12-10 09:51:34 +0000

    added content

--- 30-vpn/main.tf
+++ 30-vpn/main.tf
@@ -1,9 +1,9 @@
-resource "aws_key_pair" "vpn" {
+/* resource "aws_key_pair" "vpn" {
   key_name = "openvpnkey"
   #public_key = file("C:\\keys\\openvpn.pub") in windows
   #public_key = file("C:\\keys\\openvpn.pub") # for linux
   public_key = file("D:\\Joindevops\\daws-84s\\openvpn.pub")
-}
+} */
 
 
 resource "aws_instance" "openvpn" {
@@ -12,8 +12,8 @@ resource "aws_instance" "openvpn" {
   subnet_id                   = local.public_subnet_ids
   vpc_security_group_ids      = [local.vpn_sg_id]
   associate_public_ip_address = true
-  #key_name                    = "openvpnkey" # make sure this key exist in AWS
-  key_name  = aws_key_pair.vpn.key_name
+  key_name                    = "openvpnkey" # make sure this key exist in AWS
+  #key_name  = aws_key_pair.vpn.key_name
   user_data = file("openvpn.sh")
   tags = merge(
     var.openvpn_tags,
