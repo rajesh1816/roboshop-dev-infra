e5010c7d6130006f6f8bc51adb4d3672b87cfbfa
rajesh1816 <rajeshkaramsetty1999@gmail.com>
2025-12-10 10:06:22 +0000

    added content

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -3,10 +3,10 @@ locals {
   database_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
   vpn_sg_id          = data.aws_ssm_parameter.vpn_sg_id.value
   ami_id             = data.aws_ami.joindevops.id
-  mongodb_sg_id      = data.aws_ssm_parameter.mongodb_sg_id.value
+  /* mongodb_sg_id      = data.aws_ssm_parameter.mongodb_sg_id.value
   redis_sg_id        = data.aws_ssm_parameter.redis_sg_id.value
   rabbitmq_sg_id     = data.aws_ssm_parameter.rabbitmq_sg_id.value
-  mysql_sg_id        = data.aws_ssm_parameter.mysql_sg_id.value
+  mysql_sg_id        = data.aws_ssm_parameter.mysql_sg_id.value */
   databases_sg_id        = data.aws_ssm_parameter.databases_sg_id.value
 
 
