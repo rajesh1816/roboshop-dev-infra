a32d91bcb2219c57e988538a64c11de9014dbe3e
rajesh1816 <rajeshkaramsetty1999@gmail.com>
2025-12-10 09:08:56 +0000

    added content

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -39,7 +39,7 @@ module "backend_alb" {
   vpc_id         = local.vpc_id
 }
 
-#ingress rule for backend alb
+# Allowing backend alb from bastion
 resource "aws_security_group_rule" "backend_alb_bastion" {
   type                     = "ingress"
   from_port                = 80
@@ -47,6 +47,16 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
   protocol                 = "tcp"
   source_security_group_id = module.bastion.sg_id
   security_group_id        = module.backend_alb.sg_id
+}
+
+# allowing backend alb from vpn
+resource "aws_security_group_rule" "backend_alb_vpn" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.vpn.sg_id
+  security_group_id = module.backend_alb.sg_id
 }
 
 # backend-alb host outbound rule
@@ -91,181 +101,6 @@ resource "aws_security_group_rule" "vpn_egress_all" {
   protocol          = "-1"          # all protocols
   cidr_blocks       = ["0.0.0.0/0"] # allow to anywhere
   security_group_id = module.vpn.sg_id
-}
-
-
-#security group for mongodb
-module "mongodb" {
-  source         = "git::https://github.com/rajesh1816/terraform-sg-module.git?ref=main"
-  project        = var.project
-  environment    = var.environment
-  sg_name        = "mongodb-sg"
-  sg_description = "for mongodb"
-  vpc_id         = local.vpc_id
-}
-
-# allowing connection from vpn host to mongodb
-resource "aws_security_group_rule" "mongodb_vpn" {
-  count = length(var.mongodb_ports)
-
-  type                     = "ingress"
-  from_port                = var.mongodb_ports[count.index]
-  to_port                  = var.mongodb_ports[count.index]
-  protocol                 = "tcp"
-  source_security_group_id = module.vpn.sg_id
-  security_group_id        = module.mongodb.sg_id
-}
-
-# allowing connection from bastion host to mongodb
-resource "aws_security_group_rule" "mongodb_bastion" {
-  count                    = length(var.mongodb_ports)
-  type                     = "ingress"
-  from_port                = var.mongodb_ports[count.index]
-  to_port                  = var.mongodb_ports[count.index]
-  protocol                 = "tcp"
-  source_security_group_id = module.bastion.sg_id
-  security_group_id        = module.mongodb.sg_id
-}
-
-# mongodb host outbound rule
-resource "aws_security_group_rule" "mongodb_egress_all" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"          # all protocols
-  cidr_blocks       = ["0.0.0.0/0"] # allow to anywhere
-  security_group_id = module.mongodb.sg_id
-}
-
-
-#security group for redis
-module "redis" {
-  source         = "git::https://github.com/rajesh1816/terraform-sg-module.git?ref=main"
-  project        = var.project
-  environment    = var.environment
-  sg_name        = "redis-sg"
-  sg_description = "for redis"
-  vpc_id         = local.vpc_id
-}
-
-# allowing connection from vpn host to redis
-resource "aws_security_group_rule" "redis_vpn" {
-  count = length(var.redis_ports)
-
-  type                     = "ingress"
-  from_port                = var.redis_ports[count.index]
-  to_port                  = var.redis_ports[count.index]
-  protocol                 = "tcp"
-  source_security_group_id = module.vpn.sg_id
-  security_group_id        = module.redis.sg_id
-}
-
-# allowing connection from bastion host to redis
-resource "aws_security_group_rule" "redis_bastion" {
-  count                    = length(var.redis_ports)
-  type                     = "ingress"
-  from_port                = var.redis_ports[count.index]
-  to_port                  = var.redis_ports[count.index]
-  protocol                 = "tcp"
-  source_security_group_id = module.bastion.sg_id
-  security_group_id        = module.redis.sg_id
-}
-
-# redis host outbound rule
-resource "aws_security_group_rule" "redis_egress_all" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"          # all protocols
-  cidr_blocks       = ["0.0.0.0/0"] # allow to anywhere
-  security_group_id = module.redis.sg_id
-}
-
-
-#security group for rabbitmq
-module "rabbitmq" {
-  source         = "git::https://github.com/rajesh1816/terraform-sg-module.git?ref=main"
-  project        = var.project
-  environment    = var.environment
-  sg_name        = "rabbitmq-sg"
-  sg_description = "for rabbitmq"
-  vpc_id         = local.vpc_id
-}
-
-# allowing connection from vpn host to rabbitmq
-resource "aws_security_group_rule" "rabbitmq_vpn" {
-  count = length(var.rabbitmq_ports)
-
-  type                     = "ingress"
-  from_port                = var.rabbitmq_ports[count.index]
-  to_port                  = var.rabbitmq_ports[count.index]
-  protocol                 = "tcp"
-  source_security_group_id = module.vpn.sg_id
-  security_group_id        = module.rabbitmq.sg_id
-}
-
-# allowing connection from bastion host to rabbitmq
-resource "aws_security_group_rule" "rabbitmq_bastion" {
-  count                    = length(var.rabbitmq_ports)
-  type                     = "ingress"
-  from_port                = var.rabbitmq_ports[count.index]
-  to_port                  = var.rabbitmq_ports[count.index]
-  protocol                 = "tcp"
-  source_security_group_id = module.bastion.sg_id
-  security_group_id        = module.rabbitmq.sg_id
-}
-
-# rabbitmq host outbound rule
-resource "aws_security_group_rule" "rabbitmq_egress_all" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"          # all protocols
-  cidr_blocks       = ["0.0.0.0/0"] # allow to anywhere
-  security_group_id = module.rabbitmq.sg_id
-}
-
-#security group for mysql
-module "mysql" {
-  source         = "git::https://github.com/rajesh1816/terraform-sg-module.git?ref=main"
-  project        = var.project
-  environment    = var.environment
-  sg_name        = "mysql-sg"
-  sg_description = "for mysql"
-  vpc_id         = local.vpc_id
-}
-
-# allowing connection from vpn host to mysql
-resource "aws_security_group_rule" "mysql_vpn" {
-  count = length(var.mysql_ports)
-
-  type                     = "ingress"
-  from_port                = var.mysql_ports[count.index]
-  to_port                  = var.mysql_ports[count.index]
-  protocol                 = "tcp"
-  source_security_group_id = module.vpn.sg_id
-  security_group_id        = module.mysql.sg_id
-}
-
-# allowing connection from bastion host to mysql
-resource "aws_security_group_rule" "mysql_bastion" {
-  count                    = length(var.mysql_ports)
-  type                     = "ingress"
-  from_port                = var.mysql_ports[count.index]
-  to_port                  = var.mysql_ports[count.index]
-  protocol                 = "tcp"
-  source_security_group_id = module.bastion.sg_id
-  security_group_id        = module.mysql.sg_id
-}
-
-# mysql host outbound rule
-resource "aws_security_group_rule" "mysql_egress_all" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"          # all protocols
-  cidr_blocks       = ["0.0.0.0/0"] # allow to anywhere
-  security_group_id = module.mysql.sg_id
 }
 
 
@@ -396,6 +231,179 @@ resource "aws_security_group_rule" "catalogue_egress_all" {
 
 
 
+/* #security group for mongodb
+module "mongodb" {
+  source         = "git::https://github.com/rajesh1816/terraform-sg-module.git?ref=main"
+  project        = var.project
+  environment    = var.environment
+  sg_name        = "mongodb-sg"
+  sg_description = "for mongodb"
+  vpc_id         = local.vpc_id
+}
+
+# allowing connection from vpn host to mongodb
+resource "aws_security_group_rule" "mongodb_vpn" {
+  count = length(var.mongodb_ports)
+
+  type                     = "ingress"
+  from_port                = var.mongodb_ports[count.index]
+  to_port                  = var.mongodb_ports[count.index]
+  protocol                 = "tcp"
+  source_security_group_id = module.vpn.sg_id
+  security_group_id        = module.mongodb.sg_id
+}
+
+# allowing connection from bastion host to mongodb
+resource "aws_security_group_rule" "mongodb_bastion" {
+  count                    = length(var.mongodb_ports)
+  type                     = "ingress"
+  from_port                = var.mongodb_ports[count.index]
+  to_port                  = var.mongodb_ports[count.index]
+  protocol                 = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id        = module.mongodb.sg_id
+}
+
+# mongodb host outbound rule
+resource "aws_security_group_rule" "mongodb_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"          # all protocols
+  cidr_blocks       = ["0.0.0.0/0"] # allow to anywhere
+  security_group_id = module.mongodb.sg_id
+} */
+
+
+/* #security group for redis
+module "redis" {
+  source         = "git::https://github.com/rajesh1816/terraform-sg-module.git?ref=main"
+  project        = var.project
+  environment    = var.environment
+  sg_name        = "redis-sg"
+  sg_description = "for redis"
+  vpc_id         = local.vpc_id
+}
+
+# allowing connection from vpn host to redis
+resource "aws_security_group_rule" "redis_vpn" {
+  count = length(var.redis_ports)
+
+  type                     = "ingress"
+  from_port                = var.redis_ports[count.index]
+  to_port                  = var.redis_ports[count.index]
+  protocol                 = "tcp"
+  source_security_group_id = module.vpn.sg_id
+  security_group_id        = module.redis.sg_id
+}
+
+# allowing connection from bastion host to redis
+resource "aws_security_group_rule" "redis_bastion" {
+  count                    = length(var.redis_ports)
+  type                     = "ingress"
+  from_port                = var.redis_ports[count.index]
+  to_port                  = var.redis_ports[count.index]
+  protocol                 = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id        = module.redis.sg_id
+}
+
+# redis host outbound rule
+resource "aws_security_group_rule" "redis_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"          # all protocols
+  cidr_blocks       = ["0.0.0.0/0"] # allow to anywhere
+  security_group_id = module.redis.sg_id
+} */
+
+
+/* #security group for rabbitmq
+module "rabbitmq" {
+  source         = "git::https://github.com/rajesh1816/terraform-sg-module.git?ref=main"
+  project        = var.project
+  environment    = var.environment
+  sg_name        = "rabbitmq-sg"
+  sg_description = "for rabbitmq"
+  vpc_id         = local.vpc_id
+}
+
+# allowing connection from vpn host to rabbitmq
+resource "aws_security_group_rule" "rabbitmq_vpn" {
+  count = length(var.rabbitmq_ports)
+
+  type                     = "ingress"
+  from_port                = var.rabbitmq_ports[count.index]
+  to_port                  = var.rabbitmq_ports[count.index]
+  protocol                 = "tcp"
+  source_security_group_id = module.vpn.sg_id
+  security_group_id        = module.rabbitmq.sg_id
+}
+
+# allowing connection from bastion host to rabbitmq
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  count                    = length(var.rabbitmq_ports)
+  type                     = "ingress"
+  from_port                = var.rabbitmq_ports[count.index]
+  to_port                  = var.rabbitmq_ports[count.index]
+  protocol                 = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id        = module.rabbitmq.sg_id
+}
+
+# rabbitmq host outbound rule
+resource "aws_security_group_rule" "rabbitmq_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"          # all protocols
+  cidr_blocks       = ["0.0.0.0/0"] # allow to anywhere
+  security_group_id = module.rabbitmq.sg_id
+} */
+
+/* #security group for mysql
+module "mysql" {
+  source         = "git::https://github.com/rajesh1816/terraform-sg-module.git?ref=main"
+  project        = var.project
+  environment    = var.environment
+  sg_name        = "mysql-sg"
+  sg_description = "for mysql"
+  vpc_id         = local.vpc_id
+}
+
+# allowing connection from vpn host to mysql
+resource "aws_security_group_rule" "mysql_vpn" {
+  count = length(var.mysql_ports)
+
+  type                     = "ingress"
+  from_port                = var.mysql_ports[count.index]
+  to_port                  = var.mysql_ports[count.index]
+  protocol                 = "tcp"
+  source_security_group_id = module.vpn.sg_id
+  security_group_id        = module.mysql.sg_id
+}
+
+# allowing connection from bastion host to mysql
+resource "aws_security_group_rule" "mysql_bastion" {
+  count                    = length(var.mysql_ports)
+  type                     = "ingress"
+  from_port                = var.mysql_ports[count.index]
+  to_port                  = var.mysql_ports[count.index]
+  protocol                 = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id        = module.mysql.sg_id
+}
+
+# mysql host outbound rule
+resource "aws_security_group_rule" "mysql_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"          # all protocols
+  cidr_blocks       = ["0.0.0.0/0"] # allow to anywhere
+  security_group_id = module.mysql.sg_id
+} */
 
 
 
